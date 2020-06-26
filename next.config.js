eb54b74220120fd7d253a2f83f02904494644f2e
Ryan Bahan <ryan@ryanbahan.com>
2020-06-26 11:31:35 -0600

    revert to http

--- next.config.js
+++ next.config.js
@@ -6,11 +6,11 @@ module.exports = {
         assetPrefix: process.env.BASE_PATH || '',
         baseAPIURL: {
             dev: "http://localhost:3000",
-            prod: "https://learnablebe-env.eba-trycamvb.us-east-1.elasticbeanstalk.com",
+            prod: "http://learnablebe-env.eba-trycamvb.us-east-1.elasticbeanstalk.com",
         },
         baseURL: {
             dev: "http://localhost:8080",
-            prod: "https://learnable-git-hookup-aws.ryanbahan.vercel.app",
+            prod: "http://learnable-git-hookup-aws.ryanbahan.vercel.app",
         }
     },
 }
