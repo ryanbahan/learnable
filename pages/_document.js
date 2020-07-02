f40f84f1bdf2a45eefbb7ca3899214cd0e8a0250
Ryan Bahan <ryan@ryanbahan.com>
2020-07-02 12:56:31 -0600

    Prerender all styles on server

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,36 @@
+import React from 'react'
+import NextDocument from 'next/document'
 import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+  static async getInitialProps(ctx) {
+    const styledComponentSheet = new StyledComponentSheets()
+    const materialUiSheets = new MaterialUiServerStyleSheets()
+    const originalRenderPage = ctx.renderPage
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props =>
+            styledComponentSheet.collectStyles(
+              materialUiSheets.collect(<App {...props} />),
+            ),
+        })
+      const initialProps = await NextDocument.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [
+          <React.Fragment key="styles">
+            {initialProps.styles}
+            {materialUiSheets.getStyleElement()}
+            {styledComponentSheet.getStyleElement()}
+          </React.Fragment>,
+        ],
+      }
+    } finally {
+      styledComponentSheet.seal()
+    }
   }
 
   render() {
@@ -35,7 +57,8 @@ export default class MyDocument extends Document {
           <meta property="og:url" content="https://learnable-fe.now.sh/" />
           <meta property="og:site_name" content="Learnable" />
           <link
-            rel="stylesheet"
+            rel="preload"
+            as="style"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           {this.props.styleTags}
