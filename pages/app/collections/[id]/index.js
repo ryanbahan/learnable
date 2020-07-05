cc95cf878c4638bc4f2ad22457a152abff00fa32
Ryan Bahan <ryan@ryanbahan.com>
2020-07-05 13:46:39 -0600

    Create new folder for collections

---
+++ pages/app/collections/[id]/index.js
@@ -0,0 +1,30 @@
+import AppNav from '../../../../src/components/AppNav/AppNav'
+import Header from '../../../../src/components/Header/Header'
+import Head from 'next/head';
+import styled from 'styled-components';
+import PlaylistProvider from '../../../../src/contexts/playlistContext';
+import PlaylistsContainer from '../../../../src/components/PlaylistsContainer/PlaylistsContainer';
+
+export default function App() {
+
+    return (
+        <>
+            <Head>
+                <title>Learnable</title>
+            </Head>
+            <PlaylistProvider>
+                <Header />
+                <MainWrapper>
+                    <AppNav />
+                    <PlaylistsContainer />
+                </MainWrapper>
+            </PlaylistProvider>
+        </>
+    );
+}
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 4rem);
+`;
--- pages/app/index.js
+++ pages/app/index.js
@@ -1,15 +1,6 @@
-import AppNav from '../../src/components/AppNav/AppNav'
 import Header from '../../src/components/Header/Header'
 import Head from 'next/head';
 import styled from 'styled-components';
-import PlaylistProvider from '../../src/contexts/playlistContext';
-import PlaylistsContainer from '../../src/components/PlaylistsContainer/PlaylistsContainer';
-
-const MainWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: calc(100vh - 4rem);
-`;
 
 export default function App() {
 
@@ -18,13 +9,15 @@ export default function App() {
       <Head>
         <title>Learnable</title>
       </Head>
-      <PlaylistProvider>
       <Header />
-        <MainWrapper>
-          <AppNav />
-          <PlaylistsContainer />
-        </MainWrapper>
-      </PlaylistProvider>
+      <MainWrapper>
+      </MainWrapper>
     </>
   );
 }
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 4rem);
+`;
