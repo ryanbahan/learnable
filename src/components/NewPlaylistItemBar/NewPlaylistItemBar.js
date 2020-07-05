3ea30bce26777e02fc2a017c82bfabf2bd6638f0
Ryan Bahan <ryan@ryanbahan.com>
2020-07-05 12:24:34 -0600

    Update input and newPlaylistItemBar

--- src/components/NewPlaylistItemBar/NewPlaylistItemBar.js
+++ src/components/NewPlaylistItemBar/NewPlaylistItemBar.js
@@ -1,55 +1,12 @@
 import { useState } from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
 import Input from '../Input/Input';
-
-const Article = styled.article`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Div = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  height: 4rem;
-  width: 100%;
-`;
-
-const Button = styled.button.attrs(() => {
-  'button';
-})`
-  background: none;
-  border: none;
-  font-size: ${({ theme }) => theme.fontSizes.sm};
-  transition: ease-in-out 0.1s;
-
-  &:hover {
-    cursor: pointer;
-    color: ${({ theme }) => theme.colors.fontPrimary};
-  }
-`;
-
-const PlaylistItemCount = styled.span`
-  background-color: ${({ theme }) => theme.colors.grayLighter};
-  border-radius: 2rem;
-  -moz-border-radius: 2rem;
-  color: ${({ theme }) => theme.colors.fontPrimary};
-  display: block;
-  font-size: ${({ theme }) => theme.fontSizes.xs};
-  height: 2rem;
-  line-height: 2rem;
-  text-align: center;
-  width: 2rem;
-`;
-
-const Span = styled.span`
-  font-weight: 200;
-  font-size: 1rem;
-  color: ${({ theme }) => theme.colors.fontPrimary};
-`;
+import {
+  Article,
+  Div,
+  Button,
+  PlaylistItemCount,
+  Span,
+} from './styles'
 
 const NewPlaylistItemBar = ({
   inputActive,
@@ -96,19 +53,6 @@ const NewPlaylistItemBar = ({
       )}
     </Article>
   );
-};
-
-NewPlaylistItemBar.defaultProps = {
-  playlistItems: [],
-};
-
-NewPlaylistItemBar.propTypes = {
-  inputActive: PropTypes.bool.isRequired,
-  nextStep: PropTypes.func.isRequired,
-  playlistItems: PropTypes.arrayOf(PropTypes.object),
-  playlistItemURL: PropTypes.string,
-  setPlaylistItemURL: PropTypes.func.isRequired,
-  toggleInputActive: PropTypes.func.isRequired,
 };
 
 export default NewPlaylistItemBar;
